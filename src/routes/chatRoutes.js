e9da9469bb72b2bd5a3cd769a964b76499c4175b
Khaled <wolfy.wooolfy@gmail.com>
2025-12-18 12:34:47 +0200

    C

--- src/routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { handleChat } = require("../core/chatController");
-
-router.post("/chat", handleChat);
-
-module.exports = router;
